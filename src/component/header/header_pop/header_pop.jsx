0d40b9c3cda228044cd6466bc01f51c1ad8b45fe
seop_n <iou000@naver.com>
2021-08-01 16:57:13 +0900

    로그인 안되었을 시 팝업 UI

--- src/component/header/header_pop/header_pop.jsx
+++ src/component/header/header_pop/header_pop.jsx
@@ -13,7 +13,13 @@ const HeaderPop = ({toggleMenu}) => {
 
         <div className={styles.left_pop} onClick={onMenutoggle}>
             <div className={styles.pop_menu}>
-                <>로그인</>
+                    {/* 로그인 되었을 시 안나옴 */}
+                    <div className={styles.beforeLogin}>
+                        <span className={styles.message}>서비스를 이용하시려면 로그인 해주세요.</span>
+                        <spqn className={styles.move_login_page}>로그인 또는 회원가입</spqn>
+                    </div>
+                    {/* 로그인 되었을 시 나옴 */}
+
             </div>  
         </div>
     )
