896419be865390ee3753788e8f5daeb2de998ae3
SangGi <jackid1103@naver.com>
2021-08-01 22:22:59 +0900

    로그인 페이지 기본틀

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,15 @@
 import Header from './component/header/header';
 import styles from './app.module.css';
 import Footer from './component/footer/footer';
-import ProductList from './component/product_list/product_list';
+/* import ProductList from './component/product_list/product_list';
+ */import Login from './component/register_login/login';
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
-      <ProductList />
+      {/* <ProductList /> */}
+      <Login />
       <Footer />
       <button className={styles.float}>
         <i className="fas fa-plus"></i>
--- src/component/register_login/login.jsx
+++ src/component/register_login/login.jsx
@@ -6,9 +6,22 @@ const Login = (props) => {
 
 
     return (
-        <div className={styles.root}>
+        <div className={styles.container}>
             <div className={styles.banner}>
-                로그인
+                <span>빌려드림</span>
+            </div>
+            <div className={styles.login_input}>
+                <span>아이디</span>
+                <span>패스워드</span>
+            </div>
+            <div className={styles.login_btn}>
+                회원가입 로그인
+            </div>
+            <div className={styles.login_sns}>
+                <span>네이버</span>
+                <span>카카오</span>
+                <span>페이스북</span>
+                <span>구글</span>
             </div>
         </div>
     )
