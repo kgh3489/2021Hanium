915a4ba7376dba2c4229b5fc7cf13f4599b4a446
SangGi <jackid1103@naver.com>
2021-08-16 14:53:44 +0900

    상품 등록 페이지 컴포넌트 스위칭추가

--- src/component/product_register/product_register.jsx
+++ src/component/product_register/product_register.jsx
@@ -1,10 +1,20 @@
 import styles from './product_register.module.css';
+import { useHistory, useLocation } from 'react-router-dom';
+
 
 function ProductRegister() {
     
+    const history = useHistory();
+    
+    const goToHome = () => {
+        history.push({
+            pathname: "/2021Hanium"
+        })
+    }
+
     return (
         <div className={styles.app}>
-        1
+            <button onClick={goToHome}> 홈으로가기 </button>
         </div>
     );
 }
