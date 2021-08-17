f371784c4598599ecd442e865efeef8762b71e1c
Sang-Gi <jackid1103@naver.com>
2021-08-17 15:54:30 +0900

    상품 등록 페이지 틀 완성

--- src/component/product_register/product_register.jsx
+++ src/component/product_register/product_register.jsx
@@ -13,14 +13,31 @@ function ProductRegister() {
     }
 
     return (
-        <div className={styles.header}>
-            <div className={styles.header_left}>
-                <button className={styles.gotoHome} onClick={goToHome}><i className="fas fa-arrow-left"></i></button>
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <div className={styles.header_left}>
+                    <button className={styles.gotoHome} onClick={goToHome}><i className="fas fa-arrow-left"></i></button>
+                </div>
+                <div className={styles.title}>
+                    <h1 className={styles.title_h1}>상품 등록</h1>
+                </div>
             </div>
-            <div className={styles.title}>
-                <h1 className={styles.title_h1}>Lend</h1>
+            <div className={styles.content}>
+                <div className={styles.product_photo}>
+                    <button className={styles.photo_input}>123</button>
+                </div>
+                <div className={styles.input_content}>
+                    <input type="text" name="username" placeholder="상품 이름" />
+                    <input type="text" name="username" placeholder="자전거 종류" />
+                </div>
+                <div className={styles.product_submit}>
+                    <button className={styles.submitBtn}>상품등록하기</button>
+                </div>
+               
             </div>
         </div>
+        
+        
     );
 }
 
