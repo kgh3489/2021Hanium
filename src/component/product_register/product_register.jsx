818bbc08bc8bc2e655c1c881b2469b26b7bc446f
SangGi <jackid1103@naver.com>
2021-08-18 22:33:37 +0900

    상품 등록 페이지 스피너 추가

--- src/component/product_register/product_register.jsx
+++ src/component/product_register/product_register.jsx
@@ -14,6 +14,7 @@ function ProductRegister() {
 
     return (
         <div className={styles.container}>
+            {/* Header */}
             <div className={styles.header}>
                 <div className={styles.header_left}>
                     <button className={styles.gotoHome} onClick={goToHome}><i className="fas fa-arrow-left"></i></button>
@@ -22,18 +23,30 @@ function ProductRegister() {
                     <h1 className={styles.title_h1}>상품 등록</h1>
                 </div>
             </div>
+            {/* Content */}
             <div className={styles.content}>
+                {/* Photo */}
                 <div className={styles.product_photo}>
-                    <button className={styles.photo_input}>123</button>
+                    <input type="file"/>
+                    <button className={styles.photo_inputBtn}>사진 추가</button>
                 </div>
+                {/* Product Input */}
                 <div className={styles.input_content}>
-                    <input type="text" name="username" placeholder="상품 이름" />
-                    <input type="text" name="username" placeholder="자전거 종류" />
+                    <input className={styles.productName} type="text" placeholder="상품 이름" />
+                    <select name="" className={styles.bikeStyle}>
+                        <option value="">--자전거 종류--</option>
+                        <option value="">하이브리드</option>
+                        <option value="">MTV</option>
+                        <option value="">로드</option>
+                    </select>
+                    <input type="text" name="productPrice" placeholder="상품 가격" />
+                    <input  type="text" placeholder="상품의 상세 설명을입력하세요" />
                 </div>
+                {/* Submit */}
                 <div className={styles.product_submit}>
                     <button className={styles.submitBtn}>상품등록하기</button>
                 </div>
-               
+                
             </div>
         </div>
         
