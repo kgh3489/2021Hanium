611cb783521669faedaa12a50edd764398155936
iou000 <iou000@naver.com>
2021-09-26 17:24:52 +0900

    Header, ProductList, Footer 컴포넌트를 Main컴포넌트로 합침

---
+++ frontend/src/component/main/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import ProductList from '../product_list/product_list';
+import styles from './main.module.css';
+
+const Main = (props) => {
+        
+
+
+
+    
+    
+    return (
+        <>
+            <Header />
+            <ProductList />
+            <Footer />
+        </>
+    );
+
+};
+
+export default Main;
