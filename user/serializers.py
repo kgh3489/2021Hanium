0cbcb48129ef183a9c1e51425aca6e9a93f5d3a6
SangGi <jackid1103@naver.com>
2021-10-01 21:37:15 +0900

    상품등록 axios 구현

--- user/serializers.py
+++ user/serializers.py
@@ -3,7 +3,7 @@ from .models import UserModel
 from product.serializers import ProductSerializer
 
 class UserSerializer(serializers.ModelSerializer):
-    product_model = ProductSerializer(many=True, read_only=True)
+    # product_model = ProductSerializer(many=True, read_only=True)
     class Meta:
         model = UserModel
         fields = ('username','password','nickname','phone') # 필드 설정
