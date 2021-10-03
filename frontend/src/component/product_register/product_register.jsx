b37289287465811ce619045d73b5c9962336e9e2
iou000 <iou000@naver.com>
2021-10-03 19:47:52 +0900

    상품등록 페이지 UI 수정

--- frontend/src/component/product_register/product_register.jsx
+++ frontend/src/component/product_register/product_register.jsx
@@ -103,26 +103,30 @@ function ProductRegister() {
                     <h1 className={styles.title_h1}>상품 등록</h1>
                 </div>
             </div>
+            
+            {/* Photo */}
+            <input ref={inputImgRef}  style={{display: "none"}} onChange={handleChangeFile}
+                    type="file" className="imgInput" name="productPic" accept="image/*" multiple="multiple"/>
+            <div className={styles.product_photo}>
+            <button onClick={btnChange} className={styles.photo_inputBtn}>사진 추가</button>
+            {imgFile && <ul className={styles.img_list}>
+                    {imgBase64.map((item) => {
+                        return(
+                            <li className={styles.img_item}>
+                            <img
+                            key='id1'
+                            src={item}
+                            alt="First slide" // eslint-disable-next-line
+                            />
+                            </li>
+                        )
+                    })}
+                    </ul>
+            }
+                </div>
+
             {/* Content */}
             <form className={styles.content}>
-                {/* Photo */}
-                <div className={styles.product_photo}>
-                    <input ref={inputImgRef}  style={{display: "none"}} onChange={handleChangeFile}
-                    type="file" className="imgInput" name="productPic" accept="image/*" multiple="multiple"/>
-                    <button onClick={btnChange} className={styles.photo_inputBtn}>사진 추가</button>
-                </div>
-                {imgBase64.map((item) => {
-                    return(
-                        <img
-                        key='id1'
-                        className=""
-                        src={item}
-                        alt="First slide" // eslint-disable-next-line
-                        style={{width:"50%", height:"350px", display:"flex",  flexDirection: "row"}}
-                        />
-                        
-                    )
-                })}
                 {/* Product Input */}
                 <div className={styles.inputContent}>
                     <input className={styles.productName} name="productName" type="text" placeholder="상품 이름" />
