9bf88f1f21afb28a1eec90b3ce4ab4a8f30f31d2
Sang-Gi <jackid1103@naver.com>
2021-10-11 21:01:11 +0900

    내 상품 페이지

--- frontend/src/component/header/myproduct/myproduct.jsx
+++ frontend/src/component/header/myproduct/myproduct.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import {React, useState, useEffect } from 'react';
 import styles from './myproduct.module.css';
 import { useHistory } from 'react-router-dom';
+import Product from '../../product_list/product_item/product_item';
+
+
 
 
 const MyProdcut = () => {
@@ -13,17 +16,67 @@ const MyProdcut = () => {
             pathname: "/"
         })
     }
+
+    const[products, setProducts] = useState([]);
+    
+    const loadProducts=() => {
+        return fetch(process.env.PUBLIC_URL + "/static/data_sample.json")
+        .then(res => res.json())
+    }
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        loadProducts()
+        .then(prod => setProducts(prod["products"]))
+    }, [])
     
     return (
         <div className={styles.container}>
             <div className={styles.header}>
                 <button className={styles.gotoHome} onClick={goToHome}><i className="fas fa-arrow-left"></i></button>
             </div>
-            <div className={styles.bikeB}>
-                <h1> 빌린 자전거 </h1>
+            <div className={styles.bikeUse}>
+                <div className={styles.bikeTitle}><span>사용중인 자전거</span></div>
+                <div className={styles.products}>
+                    {
+                        products.filter(product=> {
+                            if (searchTerm == "") {
+                                return product
+                            } else if (product.product_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                return product
+                            } else if (product.product_description.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                return product
+                            }
+                        }).map(product => 
+                            <Product 
+                                key={product.id}
+                                product={product}
+                            />
+                        )
+                    }
+                </div>
             </div>
-            <div className={styles.bikeU}>
-                <h1> 올린 자전거 </h1>
+            <div className={styles.bikeRent}>
+                <div className={styles.bikeTitle}><span>등록한 자전거</span></div>
+                <div className={styles.products}>
+                    {
+                        products.filter(product=> {
+                            if (searchTerm == "") {
+                                return product
+                            } else if (product.product_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                return product
+                            } else if (product.product_description.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                return product
+                            }
+                        }).map(product => 
+                            <Product 
+                                key={product.id}
+                                product={product}
+                            />
+                        )
+                    }
+                </div>
             </div>
         </div>
 
@@ -34,3 +87,4 @@ const MyProdcut = () => {
 
 export default MyProdcut;
 // className={styles.}
+
