f6df4b2754248fc9b4a3d2df96e33f6e74a6016e
SangGi <jackid1103@naver.com>
2021-10-22 13:29:31 +0900

    merge with j1011

--- frontend/src/component/product_list/product_list.jsx
+++ frontend/src/component/product_list/product_list.jsx
@@ -1,29 +1,43 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styles from './product_list.module.css';
 import Product from './product_item/product_item';
-
+import axios from 'axios';
 
 const ProductList = ({products}) => {
+
     
-    /* const [filterDisplay, setFilterDisplay] = useState('none');
+
+    //필터 기능
+    const filterRef = useRef();
 
     const filterHandler = () => {
-        if (filterDisplay === 'none') {
-            setFilterDisplay('display');
-            console.log(filterDisplay);
-        } else {
-            setFilterDisplay('none');
-            console.log(filterDisplay);
-        }
-        
-    } */
-    
+        filterRef.current.style.display = 'block'
+    }
 
-    
+    const filterMTV = () => {
+        
+        filterRef.current.style.display = 'none'
+    }
+    const filterHybrid = () => {
+
+        filterRef.current.style.display = 'none'
+    }
+    const filterRoad = () => {
+
+        filterRef.current.style.display = 'none'
+    }
 
 
     return (
         <section className={styles.product_list}>
+            <button onClick={filterHandler} className={styles.filterBtn}><i className="fas fa-filter"></i></button>
+            <div ref={filterRef} className={styles.filterBox}>
+                <div className={styles.bikeBox}>
+                    <button onClick={filterMTV} className={styles.bikeBtn}>MTV</button>
+                    <button onClick={filterHybrid} className={styles.bikeBtn}>하이브리드</button>
+                    <button onClick={filterRoad} className={styles.bikeBtn}>로드바이크</button>
+                </div>
+            </div>
             {/* <button className={styles.btn_regiprod}>버튼</button> */}
             {
                 products.map(product => 
