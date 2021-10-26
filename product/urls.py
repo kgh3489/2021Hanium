86f7441a54b374b06b35d1a0ab6522785b10ef88
kgh <kgh3489@naver.com>
2021-10-27 01:52:44 +0900

    검색 API 구현

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
 from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'product',views.ProductViewSet) #product url 라우터에 등록
+
 
 urlpatterns = [
+    path('',include(router.urls)), #라우터를 위한 path
     path('product/', views.Product_Register_View.as_view(), name='product'),
     path('product/<int:pk>/', views.Product_Detail_View.as_view(), name='product-detail'),
     #도메인 접속 -> views의 해당 뷰의 함수 실행
--- product/views.py
+++ product/views.py
@@ -10,11 +10,12 @@ from rest_framework.decorators import permission_classes, api_view
 from rest_framework.filters import SearchFilter
 from django.http.response import HttpResponse 
 from .models import ProductModel 
-from .serializers import ProductSerializer, SerachSerializer
+from .serializers import ProductSerializer
 
 
-class Product_Register_View(APIView):
-    #permission_classes = [AllowAny]
+class Product_Register_View(APIView): #상품 등록 API
+    permission_classes = [AllowAny]
+
     def get(self, request, format=None):
         products = ProductModel.objects.all()
         serializer = ProductSerializer(products, many=True)
@@ -28,25 +29,53 @@ class Product_Register_View(APIView):
         return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
 
 
-class Product_Detail_View(APIView):
+class Product_Detail_View(APIView): #등록된 상품 상세 정보 API(CRUD)
+    permission_classes = [AllowAny]
+
     def get_object(self, pk):
-        job = get_object_or_404(ProductModel, pk=pk)
-        return job
+        product = get_object_or_404(ProductModel, pk=pk)
+        return product
 
     def get(self, request, pk):
-        job = self.get_object(pk)
-        serializer = ProductSerializer(job)
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
 
     def put(self, request, pk):
-        job = self.get_object(pk)
-        serializer = ProductSerializer(job, data=request.data)
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        job = self.get_object(pk)
-        job.delete()
+        product = self.get_object(pk)
+        product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ProductViewSet(viewsets.ModelViewSet): 
+    permission_classes = [AllowAny]
+
+    queryset = ProductModel.objects.all() 
+    serializer_class = ProductSerializer 
+
+    def get_queryset(self): # 내부에서 쿼리셋을 작성 
+        qs = super().get_queryset() # qs = qs.filter(author_id = 1) # .filter 또는 .exclude 
+        
+        # 지금 만약 로그인이 되어있다면, 로그인한 유저의 글만 필터링 
+        if self.request.user.is_authenticated: 
+            qs = qs.filter(author=self.request.user) # 만약 로그인이 되어있지 않다면, 비어있는 쿼리셋을 리턴 
+        else: 
+            qs = qs.none() 
+            return qs
+
+class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny] 
+
+    queryset = ProductModel.objects.all() 
+    serializer_class = ProductSerializer
+
+    filter_backends = [SearchFilter] 
+    search_fields = ('product_name', 'product_detail', 'product_location',)
